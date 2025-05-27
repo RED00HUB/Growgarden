24a06d8aebcbcb8e17dbdc608ac8064b12ebda7a
RED00HUB <only.risji1@gmail.com>
2025-05-27 20:43:46 +0530

    Update Grow.lua

--- Grow.lua
+++ Grow.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://scriptguardian.onrender.com/scriptguardian/files/scripts/loaders/b19b46f385f54b37b61765b3bcb32b77"))()
+loadstring(game:HttpGet('https://dpaste.com/C7SFUGJNM.txt'))()
